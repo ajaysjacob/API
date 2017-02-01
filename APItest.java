07d17b296b0276a2f46665bfe4933cb25a86713a
ajaysjacob <ajaysjacob@gmail.com>
2017-02-01 21:57:42 +0530

    Initial commit

---
+++ APItest.java
@@ -0,0 +1,13 @@
+package com.example.ajay.apitest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class APItest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apitest);
+    }
+}
